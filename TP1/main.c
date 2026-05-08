362007f2fd3a1ef9c0522a7f8b4864a9987e114a
trabaspracticosutn-tech <trabaspracticosutn@gmail.com>
2026-05-08 17:34:04 -0300

    sacando algunas cositas

--- TP1/main.c
+++ TP1/main.c
@@ -65,46 +65,40 @@ void SerialIn(void){
     }
 }
 
-uint16_t Alerta(uint16_t tmp){
+void Alerta(uint16_t tmp){
     if (tmp >= LEJANA) {
         
         pinBuffer_Out  = 0;
-        
-        return 0;
     }  
     
-    if (tmp <= LEJANA && tmp >= INTERMEDIA) {
+    if (tmp < LEJANA && tmp >= INTERMEDIA) {
         
         pinBuffer_Out  = 1;
-        __delay_ms(60);
+        __delay_ms(600);
         pinBuffer_Out  = 0;
         
-        return 1;
     } 
     
-    if (tmp <= INTERMEDIA && tmp >= CERCANA) {
+    if (tmp < INTERMEDIA && tmp >= CERCANA) {
         
         pinBuffer_Out  = 1;
-        __delay_ms(30);
+        __delay_ms(300);
         pinBuffer_Out  = 0;
         
-        return 2;
     }
     
-    if ( tmp <= CERCANA && tmp >= CRITICA){
+    if ( tmp < CERCANA && tmp >= CRITICA){
         
         pinBuffer_Out  = 1;
         __delay_ms(20);
         pinBuffer_Out  = 0;
         
-        return 3;
     }
     
-    if ( tmp <= CRITICA && tmp > 0){
+    if ( tmp < CRITICA && tmp > 0){
         
         pinBuffer_Out  = 1;
 
-        return 0;
     }
 }
 
@@ -173,23 +167,17 @@ uint16_t mediana3(uint16_t a, uint16_t b, uint16_t c)
     return c;
 }
 
-uint16_t filtro_prom(uint16_t nueva)
-{
-    static uint16_t f = 0;
-    f = (f*3 + nueva) / 4;
-    return f;
-}
 
 
 void main (void){
     uint16_t tiempo[3];
-    uint16_t distancia, dist_f;
-    uint16_t status;
+    uint16_t distancia;
+
     
     Init();
     
     while(1){
-       // SerialIn();
+        // SerialIn();
         Disparo();
         tiempo[0] = Contador();
         Disparo();
@@ -198,30 +186,18 @@ void main (void){
         tiempo[2] = Contador();
         
         distancia = mediana3(tiempo[0],tiempo[1],tiempo[2]);
-        dist_f = filtro_prom(distancia);
         
-        status = Alerta(dist_f);
+        Alerta(distancia);
         
         uart_tx_string("Dist: ");
-        uart_tx_num(dist_f);
-        uart_tx_string(" cm\n\r");
-
-        switch(status){
-            case 0: 
-                __delay_ms(100);
-                break;
-            case 1:
-                __delay_ms(40);
-                break;
-            case 2:
-                __delay_ms(70);
-                break;
-            case 3:
-                __delay_ms(80);
-                break;
-            default:
-                __delay_ms(100);
-                break;
+        
+        if (distancia == 0){
+            uart_tx_string(" Error\n\r");
+        } else {
+            uart_tx_num(distancia);
+            uart_tx_string(" cm\n\r");
         }
+        
+        
     }
 }
